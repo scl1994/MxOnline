70c2a79c05f625e1dadd773a28ab75ebff8aec2d
scl1994 <sunchenglu1994@163.com>
2017-10-27 22:54:54 +0800

    remove media directory in static directory

--- MxOnline/urls.py
+++ MxOnline/urls.py
@@ -18,8 +18,11 @@ from django.contrib import admin
 from django.views.generic import TemplateView
 import xadmin
 
+from users.views import login
+
 urlpatterns = [
     url(r'^xadmin/', xadmin.site.urls),
 
-    url("^$", TemplateView.as_view(template_name='index.html'), name='index')
+    url(r"^$", TemplateView.as_view(template_name='index.html'), name='index'),
+    url(r"^login/$", login, name='login')
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,19 @@
+# coding=utf-8
+
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
+
+
+def login(request):
+    if request.method == "POST":
+        user_name = request.POST.get("username", "")
+        pass_word = request.POST.get("password", "")
+        user = authenticate(user_name, pass_word)
+        if user is not None:
+            login(request, user)
+            return render(request, 'index.html')
+        else:
+            return render(request, "index.html", {})
+    return render(request, "login.html", {})
